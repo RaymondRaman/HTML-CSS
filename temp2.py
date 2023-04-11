616ff2c3ec439ef4470ce9e5a546b8c6ecb48f88
Raymond Li <LIKAWAI@link.cuhk.edu.hk>
2023-04-11 23:46:28 +0800

    Update temp2.py

--- temp2.py
+++ temp2.py
@@ -12,3 +12,22 @@ def sort_by_age(list1):
 #Better: Written by ChatGPT
 def sort_by_age(list1):
     return sorted(list1, key=lambda x: (x[1], x[0]))
+
+#def prefix_sum(a)
+def prefix_sum(a):
+    b = []
+    sum1 = 0
+    for i in range(len(a)):
+        sum1 += a[i]
+        b.append(sum1)
+    return b
+
+#def merge_sorted_lists(list1, list2)
+def merge_sorted_lists(list1, list2):
+    merged_list = list1 + list2
+    for i in range(len(merged_list)):
+        for j in range(i+1, len(merged_list)):
+            if merged_list[i] > merged_list[j]:
+                merged_list[i], merged_list[j] = merged_list[j], merged_list[i]
+
+    return merged_list
