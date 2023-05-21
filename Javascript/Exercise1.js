1a4800bc6d7092e37fd0a37e214bcfd909a59c1b
Raymond Li <LIKAWAI@link.cuhk.edu.hk>
2023-05-21 08:52:04 +0800

    Update Exercise1.js

--- Javascript/Exercise1.js
+++ Javascript/Exercise1.js
@@ -9,3 +9,12 @@ let testMarks = function(name, score){
 console.log(testMarks1('Santa', 99));
 console.log(testMarks('Santa', 99));
 
+// Exercise 
+// Create an event listener that shows a message when you click on the <h1> element in the HTML page below.
+// Your code goes here!
+var h1 = document.querySelector("h1");
+h1.onclick = function() {
+    alert("Hello world!")
+}
+
+
