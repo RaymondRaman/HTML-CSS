bacead54617515109ed2d7519d37c2d6fa78f2fb
Raymond Li <LIKAWAI@link.cuhk.edu.hk>
2023-05-21 16:57:58 +0800

    Update Exercise1.js

--- Javascript/Exercise1.js
+++ Javascript/Exercise1.js
@@ -112,3 +112,13 @@ var getAverages = function(students) {
   return averages;
 };
 
+// Exercise 
+// Create an additional property (named modulo) in the operations object that computes the remainder of dividing a number by another number.
+var operations = {
+  sum: function(x, y){ return x + y; }, 
+  subtract: function(x, y){ return x - y; }, 
+  multiply: function(x, y){ return x * y; }, 
+  divide: function(x, y){ return x / y; },
+  modulo: function(x, y){ return x % y}
+};
+
