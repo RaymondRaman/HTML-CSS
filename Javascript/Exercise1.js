ee24a5ee7ea5ed0c0c8f3587492322d6f51cae69
Raymond Li <LIKAWAI@link.cuhk.edu.hk>
2023-05-21 16:26:22 +0800

    Update Exercise1.js

--- Javascript/Exercise1.js
+++ Javascript/Exercise1.js
@@ -35,3 +35,18 @@ var rangeSum = function(number) {
   }
   return sum;
 };
+
+// Exercise 
+// Given an array of arrays, each of which contains a set of numbers, write a function that returns an array where each item is the sum of all the items in the sub-array.
+var arraySum = function(numbers) {
+  var sums = Array(numbers.length).fill(0); 
+  for (var i = 0; i < numbers.length; i++) {
+    for (var j = 0; j < numbers[i].length; j++) {
+      sums[i] += numbers[i][j]; 
+    }
+  }
+  return sums;
+};
+
+
+
