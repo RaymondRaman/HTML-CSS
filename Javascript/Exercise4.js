0eee0d08d62807229555ab15614b308933682ba2
Raymond Li <LIKAWAI@link.cuhk.edu.hk>
2023-05-21 21:23:54 +0800

    Create Exercise4.js

---
+++ Javascript/Exercise4.js
@@ -0,0 +1,5 @@
+// Exercise 
+// Use Element.style to change the width of the h1 element to half that of its parent container.
+// Write your code here 
+var h1 = document.querySelector('h1');
+h1.style.width = "50%";
