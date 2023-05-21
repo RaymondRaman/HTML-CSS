6e3e9dc301132184936aedb7bce32f68e604bc4a
Raymond Li <LIKAWAI@link.cuhk.edu.hk>
2023-05-21 21:32:44 +0800

    Update Exercise4.js

--- Javascript/Exercise4.js
+++ Javascript/Exercise4.js
@@ -3,3 +3,16 @@
 // Write your code here 
 var h1 = document.querySelector('h1');
 h1.style.width = "50%";
+
+// Exercise 
+// Given the following CSS definition:
+// .invisible {
+//   opacity: 0; 
+// }
+// Use methods in Element.classList to make the following picture switch between disappearing and reappearing when it is clicked:
+// Write your code here 
+var img = document.querySelector('img');
+
+img.onclick = function(){
+    this.classList.toggle('invisible');
+}
