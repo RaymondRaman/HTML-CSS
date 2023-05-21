52d3fb09b35e446efd1c92c850517a58c45b8961
Raymond Li <LIKAWAI@link.cuhk.edu.hk>
2023-05-21 17:38:23 +0800

    Create Exercise2.js

---
+++ Javascript/Exercise2.js
@@ -0,0 +1,4 @@
+// Exercise 
+// Store the <p> element below in a variable, then change the contents of the <p> element programmatically.
+var p = document.querySelector('p');
+p.innerHTML = "Modified HTML using Javascript!";
