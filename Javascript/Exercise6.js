195de2df3c381155b3a34230769faa7618428524
Raymond Li <LIKAWAI@link.cuhk.edu.hk>
2023-05-28 20:55:44 +0800

    Update Exercise6.js

--- Javascript/Exercise6.js
+++ Javascript/Exercise6.js
@@ -12,3 +12,9 @@ At the age of ${usersAge}, ${usersName} went to college at ${usersCollege}.
 ${usersName} graduated and went to work as a ${usersProfession}.
 Then, ${usersName} adopted an animal named ${usersPetName}.
 They both lived happily ever after!`);
+
+// parseInt() verifying
+let a = parseInt(prompt("Enter an integer?")); // 1st integer
+let b = parseInt(prompt("Enter another integer?")); // 2nd integer
+let result = a + b; // adding up
+console.log("When we apply + operator it gives " + result + " which is addition of two numbers!");
