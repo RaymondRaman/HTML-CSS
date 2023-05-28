67f600e49444567d3d9a58f1caf494d0379b1832
Raymond Li <LIKAWAI@link.cuhk.edu.hk>
2023-05-28 20:44:08 +0800

    Create Exercise6.js

---
+++ Javascript/Exercise6.js
@@ -0,0 +1,13 @@
+// write your code below
+let usersName = prompt("Enter your name");
+let usersAge = prompt("Enter your age");
+let usersCity = prompt("Enter your city");
+let usersCollege = prompt("Enter your college");
+let usersProfession = prompt("Enter your profession");
+let usersPetName = prompt("Enter your pet's name");
+
+console.log(`There once was a person named ${usersName} who lived in ${usersCity}.\n
+             At the age of ${usersAge}, ${usersName} went to college at ${usersCollege}.\n
+             ${usersName} graduated and went to work as a ${usersProfession}.\n
+             Then, ${usersName} adopted an animal named ${usersPetName}.\n
+             They both lived happily ever after!`);
