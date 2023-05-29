d28bc4afcbdffa3bc1afe4f58bc9446d50f1e7bc
Raymond Li <LIKAWAI@link.cuhk.edu.hk>
2023-05-29 15:08:43 +0800

    Update Exercise7.js

--- Javascript/Exercise7.js
+++ Javascript/Exercise7.js
@@ -15,3 +15,27 @@ while (leftMost <= rightMost) {
         console.log("Palindrome");
     }
 }
+
+let usersInput = [];
+let result = "";
+for(let i = 0; i < 5; i++) {
+    usersInput[i] = prompt("Enter a string: ");
+    result = result + usersInput[i] + ",";
+    if(i == 4) {
+        result = result.slice(0, -1);
+    }
+}
+
+let leftMost = 0;
+let rightMost = usersInput.length - 1;
+while(leftMost <= rightMost) {
+    if(usersInput[leftMost] != usersInput[rightMost]) {
+        console.log(`${result} is NOT a palindrome`);
+        break;
+    }
+    leftMost++;
+    rightMost--;
+    if(leftMost > rightMost) {
+        console.log(`${result} is a palindrome`);
+    }
+}
