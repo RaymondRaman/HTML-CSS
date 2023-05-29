40be8afa6a6a9a76ca63d6fb13ffd39c5ac404ef
Raymond Li <LIKAWAI@link.cuhk.edu.hk>
2023-05-29 15:38:25 +0800

    Update Exercise7.js

--- Javascript/Exercise7.js
+++ Javascript/Exercise7.js
@@ -66,3 +66,28 @@ console.log(
  Reversed array: ${reversedArray}`
 )
 
+// Write your program here
+let u = [1,2,3,4,5,6,7,8,9,10,11,12,21,11,10,9,8,7,6,5,4,3,2,1];
+let result =  `[`;
+for(let i = 0; i < u.length; i++) {
+    result = result + u[i] + ", ";
+    if(i == u.length - 1) {
+        result = result.slice(0, -2);
+        result += `]`;
+    }
+}
+
+let leftMost = 0;
+let rightMost = u.length - 1;
+while(leftMost <= rightMost) {
+    if(u[leftMost] != u[rightMost]) {
+        console.log(`${result} is NOT a palindrome!`);
+        break;
+    }
+    leftMost++;
+    rightMost--;
+    if(leftMost > rightMost) {
+        console.log(`${result} is a PALINDROME!`);
+    }
+}
+
