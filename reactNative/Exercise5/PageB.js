8899ea5f56b6fca444d1088464151e6f84073377
Raymond Li <LIKAWAI@link.cuhk.edu.hk>
2023-06-01 14:56:09 +0800

    Create PageB.js

---
+++ reactNative/Exercise5/PageB.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native'
+
+const PageB = ({navigation}) => {
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity onPress={() =>navigation.navigate("PageA")}>
+                <Text style={styles.text}>Go to PageA</Text>
+            </TouchableOpacity>
+            {
+
+            }
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+})
+
+export default PageB
